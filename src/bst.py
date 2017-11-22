53367e40096b2200eac2f8de328532a9ee0fc465
Philip Werner <philip.r.werner@gmail.com>
2017-11-22 13:17:06 -0800

    finished with bst, working on tests

--- src/bst.py
+++ src/bst.py
@@ -4,9 +4,9 @@
 class Node(object):
     """A single node."""
 
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, data, left=None, right=None):
         """Construct a new Node."""
-        self.val = val
+        self.data = data
         self.left = left
         self.right = right
 
@@ -24,24 +24,78 @@ class BST(object):
 
     def insert(self, val):
         """Create a insert method."""
-        if self.count == 0:
+        if not isinstance(val, (int, float)):
+            raise ValueError('Please enter a number')
+        if self.root is None:
             self.root = Node(val)
             self._count += 1
-        else:
-            curr = self.root
-            while curr:
-                if val < curr.val:
-                    if curr.left is None:
-                        curr.left = Node(val, curr)
-                        self._count += 1
-                        break
-                    else:
-                        curr = curr.left
-                elif val > curr.val:
-                    if curr.right is None:
-                        curr.right = Node(val, curr)
-                        self._count += 1
-                        break
-                    else:
-                        curr = curr.right
+            return
 
+        curr = self.root
+        while curr:
+            if val == curr.data:
+                raise ValueError('Node already exists')
+            elif val < curr.data:
+                if curr.left:
+                    curr = curr.left
+                else:
+                    curr.left = Node(val)
+                    self._count += 1
+                    break
+            elif val > curr.data:
+                if curr.right:
+                    curr = curr.right
+                else:
+                    curr.right = Node(val)
+                    self._count += 1
+                    break
+
+    def search(self, val):
+        """Create search method to find value in BST."""
+        if self.root is None or not isinstance(val, (int, float)):
+            return
+        curr = self.root
+        while curr:
+            if val == curr.data:
+                return curr
+            elif val < curr.data:
+                curr = curr.left
+            elif val > curr.data:
+                curr = curr.right
+
+    def size(self):
+        """Create size method to return size of bst."""
+        return self._count
+
+    def depth(self, root):
+        """Create method to return bst depth."""
+        if root is None:
+            return 0
+        if not self.root.left and not self.root.right:
+            return 0
+        elif self.root.left and not self.root.right:
+            return self.depth(self.root.left) + 1
+        elif self.root.right and not self.root.left:
+            return self.depth(self.root.right) + 1
+        else:
+            return max(self.depth(self.root.left), self.depth(self.root.right)) + 1
+
+    def contains(self, val):
+        """Create method to find if an individual node exists."""
+        if self.search(val) is not None:
+            return True
+        else:
+            return False
+
+    def balance(self):
+        """Create method that returns how the BST is balanced."""
+        if self.root is None:
+            return 'The tree is empty.'
+        elif not self.root.left and self.root.right:
+            return 0
+        elif self.root.left and not self.root.right:
+            return self.depth(self.root.left)
+        elif self.root.right and not self.root.left:
+            return self.depth(self.root.right)
+        else:
+            return self.depth(self.root.right) - self.depth(self.root.left)
--- src/test_bst.py
+++ src/test_bst.py
@@ -1,1 +1,14 @@
+"""Test module for the BST."""
 import pytest
+from bst import Node, BST
+
+
+@pytest.ficture
+def test_bst():
+    """Make an empty BST."""
+    return BST()
+
+
+def test_add_to_bst(test_bst):
+    """Test insert method."""
+    assert test_bst.insert(2)
